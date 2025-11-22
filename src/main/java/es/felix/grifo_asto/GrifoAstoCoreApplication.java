d8fc81e2f5906a5339f81f8256a17966b0dbd1ed
Felix <gatoab2@gmail.com>
2025-11-22 12:21:57 -0500

    first commit

---
+++ src/main/java/es/felix/grifo_asto/GrifoAstoCoreApplication.java
@@ -0,0 +1,13 @@
+package es.felix.grifo_asto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrifoAstoCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrifoAstoCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/felix/grifo_asto/GrifoAstoCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package es.felix.grifo_asto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrifoAstoCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
