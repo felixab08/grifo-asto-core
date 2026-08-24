80dffcbed3f633a42ddee6c56246fb838bcd8eb4
Felix <gatoab2@gmail.com>
2026-08-24 15:54:51 -0500

    feat: created aplication services

--- src/main/java/es/felix/grifo_asto/config/SecurityConfig.java
+++ src/main/java/es/felix/grifo_asto/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import es.felix.grifo_asto.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -85,6 +86,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize
+                        // Peticiones OPTIONS (Preflight)
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         // Endpoints públicos
                         .requestMatchers(
                                 "/api/auth/**",
